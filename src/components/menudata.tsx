df1b5466a3346f19d928121ff76953db350c9ec0
IAMLARK <brianireri88@gmail.com>
2023-10-03 18:46:43 +0300

    second commit

---
+++ src/components/menudata.tsx
@@ -0,0 +1,36 @@
+export const menudata = [
+   {
+    title: "Home",
+    URL:"",
+    cName:"nav-links",
+    icon:"fa-solid fa-house-user"
+   },
+
+   {
+    title: "About",
+    URL:"",
+    cName:"nav-links",
+    icon:"fa-solid fa-circle-info"
+   },
+
+   {
+    title: "Service",
+    URL:"",
+    cName:"nav-links",
+    icon:"fa-solid fa-briefcase"
+   },
+
+   {
+    title: "Contact",
+    URL:"",
+    cName:"nav-links",
+    icon:"fa-solid fa-address-book"
+   },
+
+   {
+    title: "Sign up",
+    URL:"",
+    cName:"nav-links-mobile",
+
+   },
+]
